e489c15e1db7889dc6f3894cf9a1ad975af5b879
Cabbo <92642446+CabboShiba@users.noreply.github.com>
2022-10-28 22:28:25 +0200

    Add files via upload

---
+++ TypeTestAutomator/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using System.Diagnostics;
+
+namespace TypeTestAutomator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = $"[{DateTime.Now }] TypeTestAutomator by https://github.com/CabboShiba";
+            Log("PRESS ENTER TO START...");
+            Console.ReadLine();
+            Log("Starting...");
+            Thread.Sleep(500);
+            Automation.LoadBrowser("https://www.livechat.com/typing-speed-test/#/");
+            Thread.Sleep(1000);
+            for (int i = 0; i < 507; i++)
+            {
+                Thread t = new Thread(new ThreadStart(Automation.SubmitWord));
+                t.Start();
+                Thread.Sleep(32); // best delay to never miss a word
+            }
+            Console.WriteLine();
+            Log("-------------------------------");
+            Log($"Challenge completed in: {60 - Automation.GetTime()} Seconds");
+            Log("Total WPM: " + Automation.GetWPM());
+            Log("Total CharPM: " + Automation.GetCharPM());
+            Log($"Accuracy: {Automation.GetAccuracy()}%");
+            Log("-------------------------------");
+            Automation.driver.Quit();
+            Log("Finished. Press enter to leave...");
+            Console.ReadLine();
+            Process.GetCurrentProcess().Kill();
+        }
+
+        public static void Log(string Data)
+        {
+            Console.WriteLine($"[{DateTime.Now}] {Data}");
+        }
+    }
+}
---
+++ TypeTestAutomator/Selenium/Automation.cs
@@ -0,0 +1,52 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeTestAutomator
+{
+    internal class Automation
+    {
+        public static IWebDriver driver = null;
+        public static void LoadBrowser(string Link)
+        {
+            var DeviceDriver = ChromeDriverService.CreateDefaultService();
+            DeviceDriver.HideCommandPromptWindow = true;
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("--disable-infobars");
+            options.AddArgument("--window-size=1200,1200");
+            Automation.driver = new ChromeDriver(DeviceDriver, options);
+            Automation.driver.Navigate().GoToUrl(Link);
+            Automation.driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+        }
+        public static void SubmitWord()
+        {
+            string Word = Automation.driver.FindElement(By.XPath("//*[@id=\"app\"]/div/div[2]/div[1]/div/span/div[2]/span/div/div[2]/div[2]/span[1]")).Text;
+            driver.FindElement(By.XPath("//*[@id=\"test-input\"]")).SendKeys(Word + " ");
+            Program.Log($"Obtained word: {Word}");
+        }
+        public static int GetTime()
+        {
+            int Time = int.Parse(driver.FindElement(By.XPath("//*[@id=\"app\"]/div/div[2]/div[1]/div/span/div[1]/div/div[1]/div/div[1]")).Text);
+            return Time;
+        }
+        public static int GetWPM()
+        {
+            int WPM = int.Parse(driver.FindElement(By.XPath("//*[@id=\"app\"]/div/div[2]/div[1]/div/span/div[1]/div/div[2]/div[1]/div[1]")).Text);
+            return WPM;
+        }
+        public static int GetCharPM()
+        {
+            int CPM = int.Parse(driver.FindElement(By.XPath("//*[@id=\"app\"]/div/div[2]/div[1]/div/span/div[1]/div/div[2]/div[2]/div[1]")).Text);
+            return CPM;
+        }
+        public static int GetAccuracy()
+        {
+            int Accuracy = int.Parse(driver.FindElement(By.XPath("//*[@id=\"app\"]/div/div[2]/div[1]/div/span/div[1]/div/div[2]/div[3]/div[1]")).Text);
+            return Accuracy;
+        }
+    }
+}
